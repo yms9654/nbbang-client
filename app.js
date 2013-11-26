ef79438d41da737f0b20aff66b5feca6fbe93216
minsik <yms9654@gmail.com>
2013-11-26 19:42:45 +0900

    making main view

--- app.js
+++ app.js
@@ -4,13 +4,31 @@ Ext.application({
             fullscreen: true,
             layout: 'vbox',
             items: [{
-                html: 'Awesome banner',
-                style: 'background-color: #759E60;',
+				xtype: 'container',
+				layout: 'hbox',
+                style: 'background-color: #7DB9F5;',
+				padding: 10,
+                flex: 1,
+				items: [{
+					html: 'total',
+					style: 'color: white; font-size:22px'
+				}, {
+					html: 'test2',
+					docked: 'right',
+					padding: 10
+				}]
+            }, {
+                html: 'view 2',
+                style: 'background-color: #7DB9F5;',
                 flex: 1
             }, {
-                html: 'Some wonderful information',
-                style: 'background-color: #5E99CC;',
-                flex: 2
+                html: 'view 3',
+                style: 'background-color: white;',
+                flex: 2 
+            }, {
+                html: 'view 4',
+                style: 'background-color: white;',
+                flex: 2 
             }]
         });
     } // launch
